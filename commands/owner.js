fc504ba58626a76df9862db25fb5c502de9356e9
RANKMAKER <94234333+RANKMAKER@users.noreply.github.com>
2024-04-28 15:33:34 +0100

    owner.js

--- commands/owner.js
+++ commands/owner.js
@@ -24,8 +24,7 @@ module.exports = {
 
   run: async (client, interaction) => {
     try {
-      const youtubeLink = 'https://discord.gg/2wVFGjZqQw';
-      const RobloxLink = 'https://www.roblox.com/users/691099876/profile';
+      const RobloxLink = 'https://www.roblox.com/users/471034040/profile';
       const { EmbedBuilder } = require('discord.js')
         const embed = new EmbedBuilder()
             .setColor('#da2a41')
@@ -34,7 +33,7 @@ module.exports = {
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157310253520662638/2443-iconperson.png?ex=651824aa&is=6516d32a&hm=0becc4a0fda01e5a02a63cf098db30c287e60a474f8d2da4ddeae7f47d98a5a3&',
           url: 'https://discord.gg/FUEHs7RCqz'
         })
-            .setDescription(`__**About me**__:\n\n I am Boss, a fellow Discord user, who happened to have put this bot up online./n ❤️ [YouTube](${youtubeLink})\n 💙 [Roblox](${RobloxLink})`)
+            .setDescription(`__**About me**__:\n\n I am Jack, This bot is inspired by B0ss Music. This music is for EU. 💙 [Roblox](${RobloxLink})`)
             .setTimestamp();
       interaction.reply({ embeds: [embed] }).catch(e => {});
 
